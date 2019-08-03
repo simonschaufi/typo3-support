5b12c594f828b04b5d307fea4afd7cba62ce079c
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2019-08-03 14:16:38 +0000

    initial commit

---
+++ src/Support/Jsonable.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace SimonSchaufi\TYPO3Support\Support;
+
+interface Jsonable
+{
+    /**
+     * Convert the object to its JSON representation.
+     *
+     * @param  int  $options
+     * @return string
+     */
+    public function toJson($options = 0);
+}
